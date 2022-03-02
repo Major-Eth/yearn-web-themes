a1073e33ea0f1e116c35c8b5394e9d9670d25087
Major-Eth <major-tom.eth@pm.me>
2022-03-02 10:11:00 +0100

    ✏️ use of camelCase

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
 const light = {
 	name: 'light',
 	colors: {
-		'background': '#F4F7FB',
-		'background-variant': '#E0EAFF',
-		'surface': '#FFFFFF',
-		'surface-variant': '#F9FBFD',
-		'primary': '#0657F9',
-		'primary-variant': '#004ADF',
-		'secondary': '#E0EAFF',
-		'titles': '#001746',
-		'titles-variant': '#0657F9',
-		'texts': '#7F8DA9',
-		'icons': '#CED5E3',
-		'icons-variant': '#475570'
+		background: '#F4F7FB',
+		backgroundVariant: '#E0EAFF',
+		surface: '#FFFFFF',
+		surfaceVariant: '#F9FBFD',
+		primary: '#0657F9',
+		primaryVariant: '#004ADF',
+		secondary: '#E0EAFF',
+		titles: '#001746',
+		titlesVariant: '#0657F9',
+		texts: '#7F8DA9',
+		icons: '#CED5E3',
+		iconsVariant: '#475570'
 	}
 }
 
 const dark = {
 	name: 'dark',
 	colors: {
-		'background': '#141414',
-		'background-variant': '#272727',
-		'surface': '#000000',
-		'surface-variant': '#191919',
-		'primary': '#FFFFFF',
-		'primary-variant': '#FFFFFF',
-		'secondary': '#272727',
-		'titles': '#FFFFFF',
-		'titles-variant': '#FFFFFF',
-		'texts': '#A8A8A8',
-		'icons': '#A8A8A8',
-		'icons-variant': '#FFFFFF0'
+		background: '#141414',
+		backgroundVariant: '#272727',
+		surface: '#000000',
+		surfaceVariant: '#191919',
+		primary: '#FFFFFF',
+		primaryVariant: '#FFFFFF',
+		secondary: '#272727',
+		titles: '#FFFFFF',
+		titlesVariant: '#FFFFFF',
+		texts: '#A8A8A8',
+		icons: '#A8A8A8',
+		iconsVariant: '#FFFFFF0'
 	}
 }
 
 const blue = {
 	name: 'blue',
 	colors: {
-		'background': '#012A7C',
-		'background-variant': '#001E59',
-		'surface': '#001746',
-		'surface-variant': '#012A7C',
-		'primary': '#0657F9',
-		'primary-variant': '#004ADF',
-		'secondary': '#0657F9',
-		'titles': '#FFFFFF',
-		'titles-variant': '#FFFFFF',
-		'texts': '#7F8DA9',
-		'icons': '#7F8DA9',
-		'icons-variant': '#FFFFFF'
+		background: '#012A7C',
+		backgroundVariant: '#001E59',
+		surface: '#001746',
+		surfaceVariant: '#012A7C',
+		primary: '#0657F9',
+		primaryVariant: '#004ADF',
+		secondary: '#0657F9',
+		titles: '#FFFFFF',
+		titlesVariant: '#FFFFFF',
+		texts: '#7F8DA9',
+		icons: '#7F8DA9',
+		iconsVariant: '#FFFFFF'
 	}
 }
 
